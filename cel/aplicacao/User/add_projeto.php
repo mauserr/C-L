f691f6d120ba2003bf7847e99e962a9099b8b94d
unknown <wilker-mc@hotmail.com>
2013-09-09 11:39:52 -0300

    Seguindo alteracoes e estruturacao da pasta User

--- cel/aplicacao/User/add_projeto.php
+++ cel/aplicacao/User/add_projeto.php
@@ -1,21 +1,21 @@
 <?php
 
-session_start(); 
+session_start();
 
 include("funcoes_genericas.php");
 include("httprequest.inc");
 
 // Cenário controle de acesso
-chkUser("index.php");        
+check_User("index.php");
 
 // Este script eh chamado quando ocorre uma solicitacao de inclusao
 // de novo projeto, ou quando um novo usuario se cadastra no sistema
 
 
-//Cenário  -  Cadastrar Novo Projeto 
+//Cenário  -  Cadastrar Novo Projeto
 //Objetivo:	   Permitir ao usuário cadastrar um novo projeto
 //Contexto:	   Usuário deseja incluir um novo projeto na base de dados
-//              Pré-Condição: Login  
+//              Pré-Condição: Login
 //Atores:	   Usuário
 //Recursos:	   Sistema, dados do projeto, base de dados
 //Episódios:   O Usuário clica na opção “adicionar projeto” encontrada no menu superior.
@@ -28,34 +28,37 @@ chkUser("index.php");
 //                 deste usuário, o sistema exibe uma mensagem de erro.
 
 // Chamado atraves do botao de submit
-if (isset($submit)) {    
+if (isset($submit)) {
 
 	$id_projeto_incluido = inclui_projeto($nome, $descricao);
-    
+
 	// Inserir na tabela participa
-    
+
 	if ($id_projeto_incluido != -1 )
-    {
-	    $r = bd_connect() or die("Erro ao conectar ao SGBD");
-	    $gerente = 1;
-	    $id_usuario_corrente = $_SESSION['id_usuario_corrente'];    
-	    $q = "INSERT INTO participa (id_usuario, id_projeto, gerente) VALUES ($id_usuario_corrente, $id_projeto_incluido, $gerente  )";
-	    mysql_query($q) or die("Erro ao inserir na tabela participa");
-    }
-    else
-    {
- ?>
- <html>
- <title>Erro</title>
- <body>
-    <p style="color: red; font-weight: bold; text-align: center">Nome de projeto já existente!</p>
-    <center><a href="JavaScript:window.history.go(-1)">Voltar</a></center>
- </body>
- </html>   
- <?php  	
-    	return;
-    }	    
-?>
+	{
+		$connect_bd = bd_connect() or die("Erro ao conectar ao SGBD");
+		$gerente = 1;
+		$id_usuario_corrente = $_SESSION['id_usuario_corrente'];
+		$insert_sql = "INSERT INTO participa (id_usuario, id_projeto, gerente) VALUES ($id_usuario_corrente, $id_projeto_incluido, $gerente  )";
+		mysql_query($insert_sql) or die("Erro ao inserir na tabela participa");
+	}
+	else
+	{
+		?>
+<html>
+<title>Erro</title>
+<body>
+	<p style="color: red; font-weight: bold; text-align: center">Nome de
+		projeto já existente!</p>
+	<center>
+		<a href="JavaScript:window.history.go(-1)">Voltar</a>
+	</center>
+</body>
+</html>
+<?php  	
+return;
+	}
+	?>
 
 <script language="javascript1.3">
 
@@ -65,13 +68,13 @@ self.close();
 
 <?php
 // Chamado normalmente
-} else {        
-?>
+} else {
+	?>
 
 <html>
-    <head>
-        <title>Adicionar Projeto</title>
-        <script language="javascript1.3">
+<head>
+<title>Adicionar Projeto</title>
+<script language="javascript1.3">
 
         function chkFrmVals() {
             if (document.forms[0].nome.value == "") {
@@ -92,26 +95,31 @@ self.close();
         }
 
         </script>
-    </head>
-    <body>
-        <h4>Adicionar Projeto:</h4>
-        <br>
-        <form action="" method="post" onSubmit="return chkFrmVals();">
-        <table>
-            <tr>
-                <td>Nome:</td>
-                <td><input maxlength="128" name="nome" size="48" type="text"></td>
-            </tr>
-            <tr>
-                <td>Descrição:</td>
-                <td><textarea cols="48" name="descricao" rows="4"></textarea></td>
-            <tr>
-                <td align="center" colspan="2" height="60"><input name="submit" type="submit" value="Adicionar Projeto"></td>
-            </tr>
-        </table>
-        </form>
-        <br><i><a href="showSource.php?file=add_projeto.php">Veja o código fonte!</a></i>
-    </body>
+</head>
+<body>
+	<h4>Adicionar Projeto:</h4>
+	<br>
+	<form action="" method="post" onSubmit="return chkFrmVals();">
+		<table>
+			<tr>
+				<td>Nome:</td>
+				<td><input maxlength="128" name="nome" size="48" type="text"></td>
+			</tr>
+			<tr>
+				<td>Descrição:</td>
+				<td><textarea cols="48" name="descricao" rows="4"></textarea></td>
+			
+			
+			<tr>
+				<td align="center" colspan="2" height="60"><input name="submit"
+					type="submit" value="Adicionar Projeto"></td>
+			</tr>
+		</table>
+	</form>
+	<br>
+	<i><a href="showSource.php?file=add_projeto.php">Veja o código fonte!</a>
+	</i>
+</body>
 </html>
 
 <?php
