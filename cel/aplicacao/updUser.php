7e6e59293ca2d060c7b2a19a00187cd3b9d2e8ef
Alexcortes <alex.co.alves@gmail.com>
2013-09-12 15:09:30 -0300

    Classe updUser

--- cel/aplicacao/updUser.php
+++ cel/aplicacao/updUser.php
@@ -5,16 +5,16 @@ include("funcoes_genericas.php");
 include("httprequest.inc");
 include_once("bd.inc");
 
-$id_usuario = $_SESSION['id_usuario_corrente'];
+$id_user = $_SESSION['id_usuario_corrente'];
 
-$r = bd_connect() or die("Erro ao conectar ao SGBD");
+$connectDB = bd_connect() or die("Erro ao conectar ao SGBD");
 
 
 ?>
 
 <html>
     <head>
-        <title>Alterar dados de Usuário</title>
+        <title>Alterar dados de Usuï¿½rio</title>
         <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
     </head>
 
@@ -23,21 +23,22 @@ $r = bd_connect() or die("Erro ao conectar ao SGBD");
 
 <?php
 
-// Cenário - Alterar cadastro
+// Scenario - Alter registration
 //
-//Objetivo:  Permitir ao usuário realizar alteração nos seus dados cadastrais	
-//Contexto:  Sistema aberto, Usuário ter acessado ao sistema e logado 
-//           Usuário deseja alterar seus dados cadastrais 
-//           Pré-Condição: Usuário ter acessado ao sistema	
-//Atores:    Usuário, Sistema.	
-//Recursos:  Interface	
-//Episódios: O usuário altera os dados desejados
-// 	     Usuário clica no botão de atualizar
+//Objective: Allows the user to modify his registration data	
+//Context:   System opened, User must have accessed the system and be logged 
+//           User wants to modify his registered data 
+//Preconditions: User must have accessed the system	
+//Actors:    User, System.	
+//Resources:  Interface	
+//Episodes:  The user modify the wanted data
+// 	     User clicks on the button 'Atualizar'
 
-$senha_cript = md5($senha);
-$q = "UPDATE usuario SET  nome ='$nome' , login = '$login' , email = '$email' , senha = '$senha_cript' WHERE  id_usuario='$id_usuario'";
+$encrypt_password = md5($senha);
 
-mysql_query($q) or die("<p style='color: red; font-weight: bold; text-align: center'>Erro!Login ja existente!</p><br><br><center><a href='JavaScript:window.history.go(-1)'>Voltar</a></center>");
+$update_query = "UPDATE usuario SET  nome ='$name' , login = '$login' , email = '$email' , senha = '$encrypt_password' WHERE  id_usuario='$id_user'";
+
+mysql_query($update_query) or die("<p style='color: red; font-weight: bold; text-align: center'>Erro!Login ja existente!</p><br><br><center><a href='JavaScript:window.history.go(-1)'>Voltar</a></center>");
 
 ?>
 
