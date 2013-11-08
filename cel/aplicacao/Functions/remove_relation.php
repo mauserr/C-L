437bb82c1762313c6d43dc7cd80bf6d263477567
Fillipe Oliveira Feitosa <fillfeitosa@gmail.com>
2013-11-07 23:53:12 -0200

    [Remove_relation] Separando e refatorando a função que retira a relação.

---
+++ cel/aplicacao/Functions/remove_relation.php
@@ -0,0 +1,21 @@
+<?php
+
+###################################################################
+# File: remove_relation.php
+# Purpose: Recieve a the a project id, and removes all
+# links and relatioships existents.
+###################################################################
+if (!(function_exists("remove_relation"))) {
+    function remove_relation($id_project, $id_relation){
+        $DB = new PGDB () ;
+
+        $sql6 = new QUERY ($DB) ;
+        
+        # Remove o conceito escolhido
+        $sql6->execute ("DELETE FROM relation WHERE id_relation = $id_relation") ;
+        $sql6->execute ("DELETE FROM relation_concept WHERE id_relation = $id_relation") ;
+        
+    }
+    
+}
+?>
