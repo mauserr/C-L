aa2a4949d1f2ece7bbc5e1343e4a2f89f480fcd0
Alunos <Alunos@11.10.23.235>
2013-11-18 11:44:58 -0200

    Classe "ver_pedido_relacao" - traducao de variaveis

--- cel/aplicacao/ver_pedido_relacao.php
+++ cel/aplicacao/ver_pedido_relacao.php
@@ -14,23 +14,23 @@ require_once '/Functions/check_User.php';
 check_User("index.php"); 
 
 $submit = null;
-$pedidos = null;
-$remover = null;
-$id_projeto = null;
+$orders = null;
+$remove = null;
+$id_project = null;
 
 if (isset($submit)) {
         $DB = new PGDB () ;
         $select = new QUERY ($DB) ;
         $update = new QUERY ($DB) ;
         $delete = new QUERY ($DB) ;
-        for($count = 0; $count < sizeof($pedidos); $count++)
+        for($count = 0; $count < sizeof($orders); $count++)
         {
-                 $update->execute("update pedidorel set aprovado= 1 where id_pedido = $pedidos[$count]") ;
-                 tratarPedidoRelacao($pedidos[$count]) ;
+                 $update->execute("update pedidorel set aprovado= 1 where id_pedido = $orders[$count]") ;
+                 tratarPedidoRelacao($orders[$count]) ;
         }
-      for($count = 0; $count < sizeof($remover); $count++)
+      for($count = 0; $count < sizeof($remove); $count++)
          {
-                  $delete->execute("delete from pedidorel where id_pedido = $remover[$count]") ;
+                  $delete->execute("delete from pedidorel where id_pedido = $remove[$count]") ;
          }
 ?>
 
@@ -56,14 +56,14 @@ self.close();
   </head>
 <body>
 <h2>Pedidos de Altera��o no Conjunto de Rela��es</h2>
-<form action="?id_projeto=<?=$id_projeto?>" method="post">
+<form action="?id_project=<?=$id_project?>" method="post">
 
 <?php
 
                 $DB = new PGDB () ;
                 $select = new QUERY ($DB) ;
                 $select2 = new QUERY ($DB) ;
-                $select->execute("SELECT * FROM pedidorel WHERE id_projeto = $id_projeto") ;
+                $select->execute("SELECT * FROM pedidorel WHERE id_projeto = $id_project") ;
                 if ($select->getntuples() == 0){
                       echo "<BR>Nenhum pedido.<BR>" ;
                 }else{
@@ -72,25 +72,25 @@ self.close();
                     
                     while($record != 'LAST_RECORD_REACHED'){
                         
-                            $id_usuario = $record['id_usuario'] ;
-                            $id_pedido = $record['id_pedido'] ;
-                            $tipo_pedido = $record['tipo_pedido'] ;
-                            $aprovado = $record['aprovado'] ;
+                            $id_user = $record['id_user'] ;
+                            $id_request = $record['id_request'] ;
+                            $order_type = $record['order_type'] ;
+                            $aproved = $record['aproved'] ;
                             
-                            assert($id_usuario != NULL);
-                            assert($id_pedido != NULL);
-                            assert($tipo_pedido != NULL);
-                            assert($aprovado != NULL); 
+                            assert($id_user != NULL);
+                            assert($id_request != NULL);
+                            assert($order_type != NULL);
+                            assert($aproved != NULL); 
                            
-                            assert(is_int($id_usuario));
-                            assert(is_int($id_pedido));
+                            assert(is_int($id_user));
+                            assert(is_int($id_request));
                             
-                            $select2->execute("SELECT * FROM usuario WHERE id_usuario = $id_usuario") ;
+                            $select2->execute("SELECT * FROM usuario WHERE id_usuario = $id_user") ;
                             $usuario = $select2->gofirst () ;
-                            if(strcasecmp($tipo_pedido,'remover')){?>
+                            if(strcasecmp($order_type,'remover')){?>
         
         <br>
-                <h3>O usu�rio <a  href="mailto:<?=$usuario['email']?>" ><?=$usuario['nome']?></a> pede para <?=$tipo_pedido?> a rela��o <font color="#ff0000"><?=$record['nome']?></font> <?  if(!strcasecmp($tipo_pedido,'alterar')){echo"para conceito abaixo:</h3>" ;}else{echo"</h3>" ;}?>
+                <h3>O usu�rio <a  href="mailto:<?=$usuario['email']?>" ><?=$usuario['nome']?></a> pede para <?=$order_type?> a rela��o <font color="#ff0000"><?=$record['nome']?></font> <?  if(!strcasecmp($order_type,'alterar')){echo"para conceito abaixo:</h3>" ;}else{echo"</h3>" ;}?>
                     <table>
                 <td><b>Nome:</b></td>
                 <td><?=$record['nome']?></td>
@@ -100,17 +100,17 @@ self.close();
             </tr>
         </table>
 <?php    }else{?>
-            <h3>O usu�rio <a  href="mailto:<?=$usuario['email']?>" ><?=$usuario['nome']?></a> pede para <?=$tipo_pedido?> a rela��o <font color="#ff0000"><?=$record['nome']?></font></h3>
+            <h3>O usu�rio <a  href="mailto:<?=$usuario['email']?>" ><?=$usuario['nome']?></a> pede para <?=$tipo_request?> a rela��o <font color="#ff0000"><?=$record['nome']?></font></h3>
 <?php }
-				if ($aprovado == 1)
+				if ($aproved == 1)
                 {
     			   echo "[<font color=\"#ff0000\"><STRONG>Aprovado</STRONG></font>]<BR>";
 	    		} else
                 {
-				   echo "[<input type=\"checkbox\" name=\"pedidos[]\" value=\"$id_pedido\"> <STRONG>Aprovar</STRONG>]<BR>  " ;
+				   echo "[<input type=\"checkbox\" name=\"pedidos[]\" value=\"$id_request\"> <STRONG>Aprovar</STRONG>]<BR>  " ;
 //                 echo "Rejeitar<input type=\"checkbox\" name=\"remover[]\" value=\"$id_pedido\">" ;
                 }
-                   echo "[<input type=\"checkbox\" name=\"remover[]\" value=\"$id_pedido\"> <STRONG>Remover da lista</STRONG>]" ;
+                   echo "[<input type=\"checkbox\" name=\"remover[]\" value=\"$id_request\"> <STRONG>Remover da lista</STRONG>]" ;
                    print( "<br>\n<hr color=\"#000000\"><br>\n") ;
 		    	   $record = $select->gonext () ;
 			}
